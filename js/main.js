1b5f5d75b0f62e27f9e3951d4d9798773e798a54
Vitaliy Kazak <vitaly2193@gmail.com>
2018-03-25 13:08:54 +0300

    3.3.1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
-    $('.selectric').selectric();
+    $(".selectric").selectric();
     $("[data-fancybox]").fancybox();
+    $(".btn-user-message").on('click', function (e) {
+        e.preventDefault();
+        $.fancybox.open({
+            src: '#user_new_message',
+            type: 'inline'
+        });
+    });
+    $('.performer').on('click', '.read-message', function (e) {
+        e.preventDefault();
+        $.fancybox.open({
+            src: '#user_message',
+            type: 'inline'
+        });
+    })
 });
